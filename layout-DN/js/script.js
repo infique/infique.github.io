688c09999177d43dbcf8a25f0a42d18aac4bd832
Vitalii <47926977+infique@users.noreply.github.com>
2019-06-21 12:12:55 +0300

    responsive

--- layout-DN/js/script.js
+++ layout-DN/js/script.js
@@ -1,3 +1,4 @@
+	//mobile menu
 	function myFunction() {
 		var x = document.getElementById("myLinks");
 		if (x.style.display === "block") {
@@ -6,12 +7,12 @@
 		    x.style.display = "block";
 		  }
 	}
-
+	//hamburger animate
 	document.querySelector( "#nav-toggle" )
 		.addEventListener( "click", function() {
 			this.classList.toggle( "active" );
 	});
-
+	//lnaguage change
 	$(function () {
 		   $(".nav__lang a").click(function () {
 		    $(".nav__lang a").removeClass("active");
