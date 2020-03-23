b860c706ec4672cb18db8f3c2ec04e8934c2ed0d
Vitalii Pysanyi <from.hawaii@yahoo.com>
2020-03-23 13:29:12 +0200

    JS HW1

---
+++ A-level/JS-HW1/js/index.js
@@ -0,0 +1,47 @@
+// Task 1
+ console.log("My first debug")
+
+// Task 2
+let age = 22
+let petsName = "Sebek"
+console.log(age,petsName)
+
+// Task 3
+age = 25
+console.log(age,petsName)
+
+// Task 4
+const myPetsName = "Sebek"
+let petsAge = 5
+const isDog = true
+const petsFriend = null
+
+console.log(typeof isDog,
+    typeof petsAge,
+    typeof myPetsName,
+    typeof petsFriend)
+
+// Task 5
+const student = {
+    name: "Vitalii",
+    surname: "Pysanyi",
+    isActive: true
+}
+console.log(typeof student.isActive)
+
+// Task 6
+const numbers = [1, 2, 3, 4, 5]
+const fruits = ['apple', 'mango', 'orange', 'banana', 'avocado']
+const students = ['Dima', 'Sasha', 'Lena', 'Vlad', 'Oleg']
+
+console.log(typeof numbers[0])
+console.log(typeof fruits[2])
+console.log(typeof students[4])
+
+// Task with *********
+// *
+const lastTask = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(lastTask[0], lastTask[9])
+
+// **
+console.log(student.name, student.surname, student.isActive)
